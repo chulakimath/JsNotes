5b7174d6e2833d25130e92aa6857f754a36183f4
Santosh Chulakimath <72141660+chulakimath@users.noreply.github.com>
2023-11-15 15:14:06 +0530

    Create A04.js

---
+++ A04.js
@@ -0,0 +1,39 @@
+// localStorage.clear();
+let maindiv = document.querySelector(".main");
+function localNotes(ky, val) {
+    localStorage.setItem(ky, val);
+    let notes = localStorage;
+    console.log(notes)
+    notesDisplay(notes);
+}
+document.querySelector("#addNote").addEventListener("click", () => {
+    let key = document.getElementById("key").value;
+    let value = document.getElementById("value").value;
+    document.getElementById("key").value='';
+    document.getElementById("value").value='';
+    localNotes(key, value);
+})
+document.querySelector("#getNote").addEventListener('click',()=>{
+        let notez=localStorage;
+        notesDisplay(notez);
+})
+
+function notesDisplay(notes) {
+    let divx=document.querySelector(".notesAll");
+    let tg='';
+    for(i=0;i<notes.length;i++){
+        let key=localStorage.key(i)
+        let val=localStorage.getItem(key);
+       tg=tg+`
+       <div class="card">
+       <div id="headding">
+        ${key}
+       </div>
+       <div id="tail">
+        ${val}
+       </div>
+    </div>
+       `
+    }
+    divx.innerHTML=tg;
+}
